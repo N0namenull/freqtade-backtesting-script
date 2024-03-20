290a02041123c48a26a037ccbcead11d9e2c58c4
N0namenull <k930bx@gmail.com>
2024-03-20 23:15:24 +0300

    Added EN version

--- script-en.sh
+++ script-en.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
-    echo "Этот скрипт должен быть запущен с sudo." 
-    echo "Повторный запуск скрипта с использованием sudo..."
+    echo "This script must be run with sudo." 
+    echo "Re-running the script using sudo..."
     sudo "$0" "$@"
     exit $?
 fi
@@ -19,10 +19,10 @@ echo "
 
 "
 
-echo "Список запущенных контейнеров Docker:"
+echo "List of running Docker containers:"
 docker ps --format "table {{.Names}}\t{{.Image}}"
 
-read -p "Введите имя контейнера Docker: " container_name
+read -p "Enter the name of the Docker container: " container_name
 
 default_container_name="freqtrade"
 
@@ -31,9 +31,9 @@ if [ -z "$container_name" ]; then
 fi
 
 if docker ps -a --format "{{.Names}}" | grep -q $container_name; then
-    echo "Контейнер с именем '$container_name' найден. Запускаем команду 'docker exec' для входа в контейнер..."
+    echo "Container with name '$container_name' found. Running 'docker exec' command to enter the container..."
 
-    read -p "Введите имя стратегии или несколько через пробел: " strategy_input
+    read -p "Enter the strategy name or multiple separated by space: " strategy_input
 
     num_strategies=$(echo $strategy_input | wc -w)
 
@@ -45,12 +45,12 @@ if docker ps -a --format "{{.Names}}" | grep -q $container_name; then
         strategies="--strategy-list $strategy_input"
     fi
 
-    read -p "Временной диапазон теста в формате ГГГГММДД-ГГГГММДД: " timerange
-    read -p "Укажите таймфрейм (обязательно при указании нескольких стратегий): " timeframe
-    read -p "Максимальное количество открытых позиций: " max_open_trades
-    read -p "Объем одной позиции: " stake_amount
-    read -p "Список пар в формате XYZ/ABC для спота и XYZ/ABC:HJK для фьючерсов: " p
-    read -p "Абсолютный путь к конфигу или название файла: " config
+    read -p "Enter test timerange in YYYYMMDD-YYYYMMDD format: " timerange
+    read -p "Specify timeframe (requered for multiple strategies): " timeframe
+    read -p "Maximum number of open trades: " max_open_trades
+    read -p "Amount per stake: " stake_amount
+    read -p "List of pairs in format XYZ/ABC for spot and XYZ/ABC:HJK for futures: " p
+    read -p "Absolute path to config file or filename: " config
 
     command="freqtrade backtesting $strategies"
     [[ -n $timerange ]] && command="$command --timerange $timerange"
@@ -61,11 +61,11 @@ if docker ps -a --format "{{.Names}}" | grep -q $container_name; then
     [[ -n $config ]] && command="$command --config $config"
 
     if docker exec -it $container_name bash -c "$command"; then
-        echo "Команда выполнена успешно."
+        echo "The command completed successfully."
     else
-        echo "Произошла ошибка при выполнении команды в контейнере."
+        echo "An error occurred while executing a command in the container."
     fi
     echo "$command"
 else
-    echo "Контейнер с именем '$container_name' не найден."
+    echo "Container with name '$container_name' not found."
 fi
